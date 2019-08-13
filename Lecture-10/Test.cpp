4347d5f722e09babbf734c380d05713d8ca95786
kartik <kartik.mathur10@gmail.com>
2019-08-13 15:11:01 +0530

    Lecture-10

--- Lecture-10/Test.cpp
+++ Lecture-10/Test.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 
 int main(){
-
+	int i =10;
+	float f = 10.4f;
+	int x = f;
+	float y = i;
+	cout<<i<<endl;
+	cout<<f<<endl;
+	cout<<x<<endl;
+	cout<<y<<endl;
 
 	return 0;
 }
